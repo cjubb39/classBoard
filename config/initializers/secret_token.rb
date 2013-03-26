6821e39f584dbcee9feb5f414cbdfca325b1d20a
cjubb39 <cjubb39@gmail.com>
2013-03-26 17:20:14 -0400

    Homework 1 Ruby on Rails class. Created user model with first/last name and email fields; /home displays Hello World

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassBoard::Application.config.secret_token = '2802a661b0e72a278b25901cdfa1bd026892a47bdcd2aa81365e3e171c08251ceaec31db598a0fd3a23bc2b378826d4f98d2d736043eba6fc0ca10f8c9b97c66'
